c63d4ff5ee0beeb3462932faecf280a3b81675ae
ValeriaHurova <valeriya.hurova@gmail.com>
2024-12-04 15:11:33 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+
+
 function App() {
+  
+  const[advice, setAdvice] = useState("");
+  const[show, setShow]= useState(false)
+
+  useEffect( () => {getAdvice()}, [] )
+
+
+      const getAdvice = async () => {
+      const response = await fetch (`https://bored.api.lewagon.com/api/activity/`)
+      const data = await response.json();
+      console.log(data.activity);
+      setAdvice(data.activity);
+    }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='header'>
+        <h1>No time to be bored!</h1>
+        <h2>Do you feel bored? <br></br>Click the button and choose what to do today.</h2>
+        
+      </div>
+      <div className='header'>
+        <p>{show ? advice : advice.substring(0,0)}</p>
+        <button  class="button" onClick = {() => setShow(getAdvice)}>Click Here</button>
+      </div>
+       {/* <div className='header'>
+        <p>{advice}</p>
+        <button onClick = {getAdvice}>Click Here</button>
+      </div> */}
     </div>
+
   );
 }
 
